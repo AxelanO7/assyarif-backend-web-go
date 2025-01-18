55017c62bdba3d74dd6514dfedb0c331ae5aad57
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-19 06:51:54 +0800

    chore: Refactor ShowOrders method to filter orders based on outs and update OrderUseCase initialization

--- assyarif/usecase/order.go
+++ assyarif/usecase/order.go
@@ -3,24 +3,68 @@ package usecase
 import (
 	"assyarif-backend-web-go/domain"
 	"context"
-	"fmt"
 	"time"
 )
 
 type orderUseCase struct {
 	orderRepository domain.OrderRepository
+	outRepository   domain.OutRepository
 	contextTimeout  time.Duration
 }
 
-func NewOrderUseCase(order domain.OrderRepository, t time.Duration) domain.OrderUseCase {
+func NewOrderUseCase(order domain.OrderRepository, out domain.OutRepository,
+	t time.Duration) domain.OrderUseCase {
 	return &orderUseCase{
 		orderRepository: order,
+		outRepository:   out,
 		contextTimeout:  t,
 	}
 }
 
 func (c *orderUseCase) ShowOrders(ctx context.Context) ([]domain.Order, error) {
-	return c.orderRepository.RetrieveOrders()
+	// return c.orderRepository.RetrieveOrders()
+	finalOrders := []domain.Order{}
+	resOrders, err := c.orderRepository.RetrieveOrders()
+	if err != nil {
+		return nil, err
+	}
+	if len(resOrders) == 0 {
+		return nil, nil
+	}
+	// get all outs
+	outs, err := c.outRepository.RetrieveOuts()
+	if err != nil {
+		return nil, err
+	}
+	if len(outs) == 0 {
+		return resOrders, nil
+	}
+	// for _, order := range resOrders {
+	// 	for _, out := range outs {
+	// 		if order.ID != out.Order.ID {
+	// 			finalOrders = append(finalOrders, order)
+	// 		}
+	// 	}
+	// }
+	// // remove duplicate
+	// for i := 0; i < len(finalOrders); i++ {
+	// 	for j := i + 1; j < len(finalOrders); j++ {
+	// 		if finalOrders[i].ID == finalOrders[j].ID {
+	// 			finalOrders = append(finalOrders[:j], finalOrders[j+1:]...)
+	// 			j--
+	// 		}
+	// 	}
+	// }
+	finalOrders = resOrders
+	for i := 0; i < len(finalOrders); i++ {
+		for j := 0; j < len(outs); j++ {
+			if finalOrders[i].ID == outs[j].Order.ID {
+				finalOrders = append(finalOrders[:i], finalOrders[i+1:]...)
+				i--
+			}
+		}
+	}
+	return finalOrders, nil
 }
 
 func (c *orderUseCase) AddOrder(ctx context.Context, order domain.Order) (domain.Order, error) {
@@ -48,6 +92,5 @@ func (c *orderUseCase) AddOrders(ctx context.Context, orders []domain.Order) ([]
 }
 
 func (c *orderUseCase) DeleteOrdersById(ctx context.Context, in []uint) error {
-	fmt.Println("DeleteOrdersById UseCase", in)
 	return c.orderRepository.DeleteOrdersById(in)
 }
