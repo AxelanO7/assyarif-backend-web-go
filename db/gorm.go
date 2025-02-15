c1739511c3833efc75e759ed05151adb1717cbdf
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-16 03:12:42 +0800

    ✨ feat(db): integrate new MySQL client
    - add NewMySQLClient function to create a MySQL database connection
    - use environment variables for database configuration

--- db/gorm.go
+++ db/gorm.go
@@ -21,6 +21,7 @@ func NewGormClient() {
 	NewMySQLClient()
 }
 
+
 func NewMySQLClient() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
 
